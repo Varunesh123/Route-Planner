c4d9bb0cce5df5ff01e01fd76f5808623fd6b0f0
Varunesh <varuneshpathak21@gmail.com>
2025-09-05 21:15:00 +0530

    Added Global URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = /*process.env.REACT_APP_API_URL ||*/ 'http://localhost:5000';
+const API_BASE_URL = /*process.env.REACT_APP_API_URL ||*/ 'https://route-planner-in4s.onrender.com';
 
 const api = axios.create({
   baseURL: `${API_BASE_URL}/api`,
